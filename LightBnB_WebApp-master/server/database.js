f1b49e4715bbd155b8eec47a9ec6bfcaad69c3f6
BelenMaru <bleneee@gmail.com>
2021-09-16 20:00:51 +0000

    Add new user to the database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -51,10 +51,10 @@ const getUserWithId = function(id) {
     return(result.rows);
   })
   .catch((err) => console.log(err.message))
-  // return Promise.resolve(users[id]);
+  
 }
 exports.getUserWithId = getUserWithId;
-
+// return Promise.resolve(users[id]);
 
 /**
  * Add a new user to the database.
@@ -62,12 +62,16 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query(`INSERT INTO users (name, email, password) VALUES ($1,$2,$3) RETURNING *`, [`${user.name}`,`${user.email}`,`${user.password}`])
+  .then(result => console.log(result))
+  .catch((err)=> console.log(err.message));
+  
 }
 exports.addUser = addUser;
+// const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
 
 /// Reservations
 
